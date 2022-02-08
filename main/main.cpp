036a40637c958d3b89a55dad0bf49e72a20bc830
Eldybyhtyn <name66437@gmail.com>
2022-02-08 20:29:58 +0300

    5

--- main/main.cpp
+++ main/main.cpp
@@ -1,36 +1,28 @@
 #include <iostream>
- 
-void print(const int*, const int*);
-void twice(int*, int*);
- 
-int main()
-{
-    int nums1[] = { 1, 2, 3, 4, 5 };
-    int *begin = std::begin(nums1);
-    int *end = std::end(nums1);
-    print(begin, end);
-    std::cout << std::endl;
- 
-    int nums2[] = { 1, 2, 3, 4, 5 }; 
-    begin = std::begin(nums2);
-    end = std::end(nums2);
-    twice(begin, end);
-    print(begin, end);
-    std::cout << std::endl;
+#include <typeinfo>
+
+using namespace std;
+
+void showMas();
+double* fillArr(double*, double*);
+
+int main() {
+    
+    double mas[2];
+    double *end = std::end(mas);
+    fillArr(mas, end);
     return 0;
 }
- 
-void print(const int *begin, const int *end)
-{
-    for (const int *ptr  = begin; ptr != end; ptr++)
-    {
-        std::cout << *ptr << "\t";
-    }
+
+void showMas() {
+    cout << "Hello world!" << endl;
 }
-void twice(int *begin, int *end)
-{
-    for (int *ptr = begin; ptr != end; ptr++)
-    {
-        *ptr = *ptr * 2;
+
+double* fillArr(double *ptr, double *end) {
+    for (double *ptr2 = ptr; ptr2 < end; ptr2++) {
+        cin >> *ptr2;
     }
+
+    return ptr;
+    
 }
