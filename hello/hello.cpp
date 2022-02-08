e316335fe3ebeedf3ead898d454c5fb96ea7852a
Eldybyhtyn <name66437@gmail.com>
2022-02-08 21:41:31 +0300

    6

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,48 +1,12 @@
 #include <iostream>
-#include <math.h>
-using std::cout;
-using std::cin;
-
-double maxelem(double mas[], int length) {
-    
-    double max = *mas;
-    for (int i = 1; i < length; i++) {
-        if (*(mas + i) > max) {
-            max = *(mas + i);
-        }
-    }
-
-    return max;
-}
-
-void showarray(double *mas, int length) {
-    for (int i = 0; i < length; i++) {
-        cout << *(mas + i) << " ";
-        
-    }
-}
-
-
 
 int main() {
-	
-    
-	srand(time(NULL));
-
-    double mas[20];
-
-    for (int i = 0; i < 20; i++) {
-        mas[i] = rand() % 20;
-    }
-
-    cout << maxelem(mas, sizeof(mas) / sizeof(double)) << std::endl;
-
-    
-
-    showarray(mas, sizeof(mas) / sizeof(double));
 
 
+    int *ptr = new int(0x16b7d5a6c70);
 
-	return 0;
+    std::cout << *ptr << std::endl;
+    delete ptr;
+
+    return 0;
 }
-
--- main/main.cpp
+++ main/main.cpp
@@ -1,28 +1,14 @@
 #include <iostream>
-#include <typeinfo>
 
-using namespace std;
+using std::cout;
+using std::endl;
 
-void showMas();
-double* fillArr(double*, double*);
 
 int main() {
-    
-    double mas[2];
-    double *end = std::end(mas);
-    fillArr(mas, end);
+
+    int *ptr = new int(23);
+    cout << ptr << endl;
+    delete ptr;
+
     return 0;
-}
-
-void showMas() {
-    cout << "Hello world!" << endl;
-}
-
-double* fillArr(double *ptr, double *end) {
-    for (double *ptr2 = ptr; ptr2 < end; ptr2++) {
-        cin >> *ptr2;
-    }
-
-    return ptr;
-    
 }
