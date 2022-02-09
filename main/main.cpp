b277dc3009819ef54b4e86d60cd3c4c196173887
Eldybyhtyn <name66437@gmail.com>
2022-02-09 14:29:18 +0300

    7

--- main/main.cpp
+++ main/main.cpp
@@ -1,14 +1,26 @@
 #include <iostream>
+#include <string>
 
 using std::cout;
 using std::endl;
 
+struct Human {
+    int age = 5;
+    std::string name = "dsf";
+    std::string surname = "fasd";
+
+    struct arms {
+        bool leftArm = true;
+        bool rightArm = true;
+    };
+
+};
 
 int main() {
 
-    int *ptr = new int(23);
-    cout << ptr << endl;
-    delete ptr;
+    Human human1 {6, "lox", "togelox"};
+
+    human1.
 
     return 0;
 }
